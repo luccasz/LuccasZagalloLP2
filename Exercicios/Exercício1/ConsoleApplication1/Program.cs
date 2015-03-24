f5d2e87db924eeaece72f87dca504f8196fab341
luccasz <luccas_zagallo@hotmail.com>
2015-03-24 17:51:06 -0300

    Exercício 1

---
+++ Exercicios/Exercício1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite números para média para concluir:");
+            var num = int.Parse(Console.ReadLine());
+            int soma = 0;
+            int quant = 0;
+            double media = 0;
+ 
+            while (num > 0)
+            {   
+                quant++;
+                soma += num;
+                num = int.Parse(Console.ReadLine());
+            }
+            
+            media = soma / quant;
+            Console.WriteLine("Média: {0}", media);
+        }
+    }
+}
