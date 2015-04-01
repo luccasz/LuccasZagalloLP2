858c5954a02b0a122c7b03ba1b806e2f7ba1a80f
luccas <luccas_zagallo@hotmail.com>
2015-04-01 19:13:39 -0300

    Aula 31/03/15

---
+++ Exercicios/Aula_310315/Aula_310315/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_310315
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o coeficiente");
+            int coeficiente = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o expoente");
+            int expoente = int.Parse(Console.ReadLine());
+
+            int resultado = 1;
+            if (coeficiente == 0)
+            {
+                resultado = 0;
+            }
+            else
+                for (int i = 0; i < expoente; i++)
+                {
+                    resultado *= coeficiente;
+                }
+            Console.WriteLine(resultado);         
+            Console.ReadKey();
+        }
+
+    }
+}
