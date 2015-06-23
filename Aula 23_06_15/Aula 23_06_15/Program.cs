3e94ea87c07353df407658c45ead964e3837a5a4
luccasz <luccasz555@gmail.com>
2015-06-23 17:45:43 -0300

    Aula de Orientação Objeto

---
+++ Aula 23_06_15/Aula 23_06_15/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_23_06_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] funcionario = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                string cpf = Console.ReadLine();
+                int dtNacimento = int.Parse(Console.ReadLine());
+                double salario = int.Parse(Console.ReadLine());
+                int dtAdmissao = int.Parse(Console.ReadLine()); 
+
+                if(dtAdmissao >= 2014)
+                {
+                    double aumento = 423.45;
+                    salario = salario + aumento;
+                    Console.WriteLine("{0} ficou com {1} ", nome, salario);
+                }
+                if(dtAdmissao > 2009 && dtAdmissao < 2014)
+                {
+                    double aumento = 9.84 / 100;
+                    salario = salario + aumento;
+                    Console.WriteLine("{0}  ficou com  {1} ", nome, salario);
+                }
+                if (dtAdmissao > 1999 && dtAdmissao < 2010)
+                {
+                    double aumento = 16.4 / 100;
+                    salario = salario + aumento;
+                    Console.WriteLine("{0} ficou com {1} ", nome, salario);
+                }
+                if (dtAdmissao <= 1999)
+                {
+                    double aumento = 22.6 / 100;
+                    salario = salario + aumento;
+                    Console.WriteLine("{0} ficou com {1} ", nome, salario);
+                }
+            }
+
+        }
+    }
+}
