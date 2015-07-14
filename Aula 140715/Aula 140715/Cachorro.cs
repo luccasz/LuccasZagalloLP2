f6a10b37fc6952a7a9d0c65fc10c42986ff79357
luccasz <luccas_zagallo@hotmail.com>
2015-07-14 17:32:19 -0300

    Aula Encapsulamento

---
+++ Aula 140715/Aula 140715/Cachorro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_140715
+{
+    class Cachorro
+    {
+        public double tamanhoCm;
+        private string _nome;
+        private double _peso; 
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
+        public void SetPeso(double peso)
+        {
+            _peso = peso;
+        }
+        public double GetPeso()
+        {
+            return _peso;
+        }
+        public double RacaoDiariaKg()
+        {
+            return (_peso / (tamanhoCm / 100) * (tamanhoCm / 100))/10;
+        }
+    }
+}
---
+++ Aula 140715/Aula 140715/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_140715
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro fido = new Cachorro();
+            fido.tamanhoCm = 95; // 1 cm = 0,01 m
+            fido.SetNome("Fido");
+            fido.SetPeso(42.3);
+
+            Console.WriteLine("Nome: {0}", fido.GetNome());
+             Console.WriteLine("Peso: {0}", fido.GetPeso());
+             Console.WriteLine("Tamanho (m): {0}", fido.tamanhoCm/100);
+
+            // qtd ração = peso(kg) / tamanho(m)²
+            Console.WriteLine("Quantidade diária de ração: {0}", fido.RacaoDiariaKg());   
+
+        }
+    }
+}
