b99e7bbddf11a24fc71d2bfa2addcc1c22fe83b4
luccas <luccas_zagallo@hotmail.com>
2015-07-20 15:27:02 -0300

    Mudança no Exercício 9

--- Aula/Aula 260615 (Orientação Objeto)/Aula 260615/Program.cs
+++ Aula/Aula 260615 (Orientação Objeto)/Aula 260615/Program.cs
@@ -13,19 +13,22 @@ namespace Aula_260615
             int n = int.Parse(Console.ReadLine());
             Funcionario f = new Funcionario();
 
-            f.nome = Console.ReadLine();
-            f.cpf = Console.ReadLine();
-            f.dtNascimento = DateTime.Parse(Console.ReadLine());
-            f.dtAdmissao = DateTime.Parse(Console.ReadLine());
-            f.salario = double.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                f.nome = Console.ReadLine();
+                f.cpf = Console.ReadLine();
+                f.dtNascimento = DateTime.Parse(Console.ReadLine());
+                f.dtAdmissao = DateTime.Parse(Console.ReadLine());
+                f.salario = double.Parse(Console.ReadLine());
 
-            Console.WriteLine(" ");
-            Console.WriteLine("Nome: {0} ", f.nome);
-            Console.WriteLine("CPF: {0} ", f.cpf);
-            Console.WriteLine("Nascimento: {0} ", f.dtNascimento);
-            Console.WriteLine("Admissão: {0} ", f.dtAdmissao);
-            Console.WriteLine("Salário: {0} ", f.salario);
-            Console.WriteLine("Novo Salário: {0}", f.CalculaAumento());
+                Console.WriteLine(" ");
+                Console.WriteLine("Nome: {0} ", f.nome);
+                Console.WriteLine("CPF: {0} ", f.cpf);
+                Console.WriteLine("Nascimento: {0} ", f.dtNascimento);
+                Console.WriteLine("Admissão: {0} ", f.dtAdmissao);
+                Console.WriteLine("Salário: {0} ", f.salario);
+                Console.WriteLine("Novo Salário: {0}", f.CalculaAumento());
+            }
 
             //Console.WriteLine(f.InicialDoNome());       
             
