87c7513be699710a6cfca7c295e63426dde32e4d
luccas <luccas_zagallo@hotmail.com>
2015-07-30 19:09:35 -0300

    Exercicio11 Viagem

---
+++ Exercicios/Exercicio11 Viagem/Exercicio11 Viagem/Carro.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11_Viagem
+{
+    public class Carro
+    {
+        public Carro(int _id, string _modelo, string _marca, string _placa, int _capacidade)
+        {
+            this.p_id = _id;
+            this.p_modelo = _modelo;
+            this.p_marca = _marca;
+            this.p_placa = _placa;
+            this.p_capacidade = _capacidade;
+            this.p_ocupacao = _capacidade;
+        }
+
+        private int p_id = -1;
+        private string p_modelo = String.Empty;
+        private string p_marca = String.Empty;
+        private string p_placa = String.Empty;
+        private int p_ocupacao = 0;
+        private int p_capacidade = 0;
+
+        public int ID
+        {
+            get { return p_id; }
+            set { p_id = value; }
+        }
+
+        public string modelo
+        {
+            get { return p_modelo; }
+            set { p_modelo = value; }
+        }
+
+        public string marca
+        {
+            get { return p_marca; }
+            set { p_marca = value; }
+        }
+
+        public string placa
+        {
+            get { return p_placa; }
+            set { p_placa = value; }
+        }
+
+        public int ocupacao
+        {
+            get { return p_ocupacao; }
+            set { p_ocupacao = value; }
+        }
+
+        public int capacidade
+        {
+            get { return p_capacidade; }
+            set
+            {
+                p_capacidade = value;
+                p_ocupacao = value;
+            }
+        }
+
+        public string AlocaPassageiro()
+        {
+            if (p_ocupacao == 0)
+            {
+                return "Carro lotado!!";
+            }
+            else 
+            {
+                p_ocupacao--;
+                return "Alocação realizada";
+            }
+        }
+    }
+}
---
+++ Exercicios/Exercicio11 Viagem/Exercicio11 Viagem/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11_Viagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Carro> listaCarro = new List<Carro>();
+            Carro car;
+
+            string status = "";
+            int idCarro = 0;
+            int numCarros = 0;
+
+            bool continua = true;
+            bool carroExiste = true; 
+
+            string modelo = "";
+            string marca = "";
+            string placa = "";
+            int capacidade = 0;
+            
+            int totalPessoas = 0;
+
+            numCarros = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numCarros; i++)
+            {
+                idCarro = i + 1;
+                modelo = Console.ReadLine();
+                marca = Console.ReadLine();
+                placa = Console.ReadLine();
+                capacidade = int.Parse(Console.ReadLine());
+
+                listaCarro.Add(new Carro(idCarro, modelo, marca, placa, capacidade));
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Carros cadastrados: ");
+
+            listaCarro.ForEach(delegate(Carro p_carro)
+            {
+                Console.WriteLine(p_carro.ID + " - " + p_carro.modelo +  "," + p_carro.marca + "," + p_carro.placa + "," + p_carro.capacidade + "," + p_carro.ocupacao);
+            });
+            Console.WriteLine("");
+
+            continua = true;
+
+            while (continua == true)
+            {
+                idCarro = int.Parse(Console.ReadLine());
+
+                if (idCarro < 0)
+                {
+                    continua = false;
+                }
+                else
+                {
+                    carroExiste = listaCarro.Exists(x => x.ID == idCarro);
+
+                    if (carroExiste == true)
+                    {
+                        car = listaCarro.Find(delegate(Carro p1) { return p1.ID == idCarro; });
+
+                        status = car.AlocaPassageiro();
+
+                        Console.WriteLine(status);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Carro Inválido!!!");
+                    }
+                }
+            }
+            Console.WriteLine(status);
+
+            Console.WriteLine("Relatório Final:");
+            totalPessoas = 0;
+            listaCarro.ForEach(delegate(Carro p_carro)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Carro " + p_carro.ID + ": " + p_carro.modelo + " - " + p_carro.marca + "(" + p_carro.placa + ")");
+                Console.WriteLine("Passageiros: " + p_carro.capacidade);
+                Console.WriteLine("Vagas restantes: " + p_carro.ocupacao);
+
+                totalPessoas = totalPessoas + p_carro.capacidade - p_carro.ocupacao;
+            });
+            Console.WriteLine("");
+            Console.WriteLine("Total de Pessoas na Viagem : " + totalPessoas);
+
+            Console.ReadKey();
+        }
+    }
+}
+
