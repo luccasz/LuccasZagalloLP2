6f97aa62d515973b4a9c754eb7901267658e146a
luccasz <luccas_zagallo@hotmail.com>
2015-08-25 17:27:18 -0300

    Aula 25/08

---
+++ Aula/Aula 250815/ConsoleApplication1/Atleta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula250815
+{
+    public class Atleta
+    {
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public int TempoCs { get; set; }
+
+        public string TempoFormatado
+        {
+            get {
+                return String.Format("{0}: {1}:{2}", TempoCs / 6000, (TempoCs % 6000) / 100, (TempoCs % 6000) % 100);
+            }
+        }
+    }
+}
