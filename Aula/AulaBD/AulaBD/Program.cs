016e12d825f7e918cd1df2b28e76eea43a230dbf
luccasz <luccas_zagallo@hotmail.com>
2015-09-22 18:10:24 -0300

    Aula BD

---
+++ Aula/AulaBD/AulaBD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace AulaBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um nome para o Banco de Dados Pessoa");
+            string nome = Console.ReadLine();
+
+            string strConn = "Server=localhost;Database=test;Uid=root;Pwd=";
+
+            MySqlConnection conn = new MySqlConnection(strConn);
+
+            MySqlCommand command = new MySqlCommand();
+
+            command.Connection = conn;
+
+            command.CommandText = String.Format("INSERT INTO PESSOA (Nome) VALUES ('{0}')", nome); //somente texto pede aspas simples. double e int é sem aspas
+             
+            conn.Open();
+
+            command.ExecuteNonQuery();
+
+            conn.Close();       
+    
+        }
+    }
+}
