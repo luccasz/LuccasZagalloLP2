4a13e8457dc0b30e33b6d01995f530b128c7c128
luccasz <luccas_zagallo@hotmail.com>
2015-10-06 18:00:08 -0300

    Aula 06_10_15

---
+++ Aula/Aula 06_10_15/Aula 06_10_15/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Aula_06_10_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtPre1 = new System.Windows.Forms.TextBox();
+            this.txtPre2 = new System.Windows.Forms.TextBox();
+            this.txtPre3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnStatus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(107, 35);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(182, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(107, 72);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(182, 20);
+            this.txtSobrenome.TabIndex = 1;
+            // 
+            // txtPre1
+            // 
+            this.txtPre1.Location = new System.Drawing.Point(107, 124);
+            this.txtPre1.Name = "txtPre1";
+            this.txtPre1.Size = new System.Drawing.Size(145, 20);
+            this.txtPre1.TabIndex = 2;
+            // 
+            // txtPre2
+            // 
+            this.txtPre2.Location = new System.Drawing.Point(107, 163);
+            this.txtPre2.Name = "txtPre2";
+            this.txtPre2.Size = new System.Drawing.Size(145, 20);
+            this.txtPre2.TabIndex = 3;
+            // 
+            // txtPre3
+            // 
+            this.txtPre3.Location = new System.Drawing.Point(107, 205);
+            this.txtPre3.Name = "txtPre3";
+            this.txtPre3.Size = new System.Drawing.Size(145, 20);
+            this.txtPre3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sobrenome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Presente 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Presente 2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Presente 3:";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(107, 244);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(182, 46);
+            this.btnCadastrar.TabIndex = 10;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.Location = new System.Drawing.Point(325, 244);
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(89, 46);
+            this.btnStatus.TabIndex = 11;
+            this.btnStatus.Text = "STATUS";
+            this.btnStatus.UseVisualStyleBackColor = true;
+            this.btnStatus.Click += new System.EventHandler(this.btnStatus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 326);
+            this.Controls.Add(this.btnStatus);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPre3);
+            this.Controls.Add(this.txtPre2);
+            this.Controls.Add(this.txtPre1);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtPre1;
+        private System.Windows.Forms.TextBox txtPre2;
+        private System.Windows.Forms.TextBox txtPre3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnStatus;
+    }
+}
+
---
+++ Aula/Aula 06_10_15/Aula 06_10_15/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Aula_06_10_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Amigo> amigos = new List<Amigo>();
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Amigo c = new Amigo();
+            c.Nome = txtNome.Text;
+            c.Sobrenome = txtSobrenome.Text;
+            c.Presente1 = txtPre1.Text;
+            c.Presente2 = txtPre2.Text;
+            c.Presente3 = txtPre3.Text;
+
+            amigos.Add(c);
+        }
+        public void Registro()
+        {
+            foreach (var item in amigos)
+            {
+                MySqlConnection conn = new MySqlConnection("Server=localhost;Database=test;Uid=root;Pwd=");
+
+                MySqlCommand command = new MySqlCommand();
+
+                command.Connection = conn;
+
+                string q = String.Format("INSERT INTO AMIGO (Nome, Sobrenome, Presente1, Presente2, Presente3) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')", item.Nome, item.Sobrenome, item.Presente1, item.Presente2, item.Presente3);
+
+                command.CommandText = q;
+
+                conn.Open();
+
+                command.ExecuteNonQuery();
+
+                conn.Close();
+            }
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Foram encontrados " +  amigos.Count + " registros");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Registro();
+        }
+
+    }
+}
